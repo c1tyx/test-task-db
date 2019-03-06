6d8491a3de2a38141fbd985f4cef2732df0db9a3
Tony Melnyk <anton_mel95@ukr.net>
2019-03-07 00:27:16 +0200

    Project last changes! (c) Tony

--- db-test.local/index.php
+++ db-test.local/index.php
@@ -25,9 +25,8 @@ if(isset($_POST["register"])){
         $message = "All fields are required!";
     }
 }
-// Ругаемся, если соединение установить не удалось
 if (!$link) {
-    echo 'Не могу соединиться с БД. Код ошибки: ' . mysqli_connect_errno() . ', ошибка: ' . mysqli_connect_error();
+    echo 'Не можу підключитись до БД. Код помилки: ' . mysqli_connect_errno() . ', помилка: ' . mysqli_connect_error();
     exit;
 }
 
@@ -62,50 +61,50 @@ if (isset($_GET['del_id'])) { //проверяем, есть ли перемен
 //Если передана переменная red_id, то надо обновлять данные. Для начала достанем их из БД
 if (isset($_GET['red_id'])) {
     $sql = mysqli_query($link, "SELECT `id`, `name`, `email`, `country` FROM `users` WHERE `ID`={$_GET['red_id']}");
-    $product = mysqli_fetch_array($sql);
+    $user = mysqli_fetch_array($sql);
 }
 ?>
     <div class="container">
         <div class="row">
-    <form action="" method="post" class="form_edit">
-        <table>
-            <tr>
-                <td>Name:</td>
-                <td><input type="text" name="name"  value="<?= isset($_GET['red_id']) ? $product['name'] : ''; ?>"></td>
-                <td>Email:</td>
-                <td><input type="text" name="email"  value="<?= isset($_GET['red_id']) ? $product['email'] : ''; ?>"></td>
-                <td>Country:</td>
-                <td><input type="text" name="country"  value="<?= isset($_GET['red_id']) ? $product['country'] : ''; ?>"> </td>
-                <td><button type="submit" class="button">Підтвердження</button></td>
-            </tr>
-        </table>
-    </form>
+            <form action="" method="post" class="form_edit">
+                <table>
+                    <tr>
+                        <td>Name:</td>
+                        <td><input type="text" name="name"  value="<?= isset($_GET['red_id']) ? $user['name'] : ''; ?>"></td>
+                        <td>Email:</td>
+                        <td><input type="text" name="email"  value="<?= isset($_GET['red_id']) ? $user['email'] : ''; ?>"></td>
+                        <td>Country:</td>
+                        <td><input type="text" name="country"  value="<?= isset($_GET['red_id']) ? $user['country'] : ''; ?>"> </td>
+                        <td><button type="submit" class="button">Підтвердження</button></td>
+                    </tr>
+                </table>
+            </form>
 
-    <table class="table table-striped table-hover">
-        <thead class="thead-dark">
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Country</th>
-            <th scope="col">Delete</th>
-            <th scope="col">Edit</th>
-        </tr>
-        </thead>
-        <?php
-        $sql = mysqli_query($link, 'SELECT `id`, `name`, `email`, `country` FROM `users`');
-        while ($result = mysqli_fetch_array($sql)) {
-            echo '<tr>' .
-                "<td>{$result['id']}</td>" .
-                "<td><i class=\"fas fa-signature\"></i> {$result['name']}</td>" .
-                "<td><i class=\"fas fa-envelope\"></i> {$result['email']}</td>" .
-                "<td><i class=\"fas fa-globe\"></i> {$result['country']}</td>" .
-                "<td><i class=\"fas fa-user-minus\"></i> <a class='delete' href='?del_id={$result['id']}'>Delete this user</a></td>" .
-                "<td><i class=\"fas fa-user-edit\"></i> <a class='edit_u' href='?red_id={$result['id']}'>Edit this user</a></td>" .
-                '</tr>';
-        }
-        ?>
-    </table>
+            <table class="table table-striped table-hover">
+                <thead class="thead-dark">
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Country</th>
+                    <th scope="col">Delete</th>
+                    <th scope="col">Edit</th>
+                </tr>
+                </thead>
+                <?php
+                $sql = mysqli_query($link, 'SELECT `id`, `name`, `email`, `country` FROM `users`');
+                while ($result = mysqli_fetch_array($sql)) {
+                    echo '<tr>' .
+                        "<td>{$result['id']}</td>" .
+                        "<td><i class=\"fas fa-signature\"></i> {$result['name']}</td>" .
+                        "<td><i class=\"fas fa-envelope\"></i> {$result['email']}</td>" .
+                        "<td><i class=\"fas fa-globe\"></i> {$result['country']}</td>" .
+                        "<td><i class=\"fas fa-user-minus\"></i> <a class='delete' href='?del_id={$result['id']}'>Видалити цього користувача</a></td>" .
+                        "<td><i class=\"fas fa-user-edit\"></i> <a class='edit_u' href='?red_id={$result['id']}'>Редагувати цього користувача</a></td>" .
+                        '</tr>';
+                }
+                ?>
+            </table>
         </div>
     </div>
 
